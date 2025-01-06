2abeba3eccd1eda9941139cf16d5365eb2fdb1a4
alic3dev <alic3dev@gmail.com>
2025-01-06 01:56:31 +0000

    Remove unused declarations from `snake.h`

--- include/snake.h
+++ include/snake.h
@@ -4,15 +4,10 @@
 #include "position.h"
 
 int main();
-void interupt_handler(int);
 void* get_user_input();
 struct position* place_apple(
   struct position**,
   size_t,
-  struct position*
-);
-void initialize_buffer(
-  unsigned char*, 
   struct position*
 );
 
